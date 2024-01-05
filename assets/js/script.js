774471704bbafc40e75b7d9de9be6ab19ee1632d
BillyArshad <>
2024-01-05 19:28:18 +0000

    polishing my codes. Still in work in progress.

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,7 @@ const questions = [
         question: "Which company that owns Activision-Blizzard?",
         answers: [
         {text: "Sony", correct: false },
-        {text: "Amazon", correct: true },
+        {text: "Amazon", correct: false },
         {text: "Nintendo", correct: false },
         {text: "Microsoft", correct: true },
     ]
@@ -91,7 +91,9 @@ function showQuestion(){
         button.classList.add("btn");
 
         answerButtons.appendChild(button);
-
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
         button.addEventListener("click", selectAnswer);
     });
 }
@@ -102,5 +104,47 @@ function resetState(){
         answerButtons.removeChild(answerButtons.firstChild);
     }
 }
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true; 
+    })
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block"
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    } 
+})
 
 startQuiz();
