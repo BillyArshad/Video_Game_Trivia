bf19c908c87e6d10c47b45349c09713c78617668
BillyArshad <>
2024-01-06 12:39:32 +0000

    cleaned my codes up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,105 +1,52 @@
-const questions = [
-    {
-        question: "Which is the most successful battle royale game?",
-        answers: [
-            { text: "Call of Duty: Warzone", correct: false },
-            { text: "Fortnite", correct: true },
-            { text: "Apex Legends", correct: false },
-            { text: "Royal Realm", correct: false },
-        ]
-    },
-    {
-        question: "Which is the most popular Fallout game, according to the community?",
-        answers: [
-            { text: "Fallout: New Vegas", correct: true },
-            { text: "Fallout 4", correct: false },
-            { text: "Fallout 2", correct: false },
-            { text: "Fallout 76", correct: false },
-        ]
-    },
-    {
-        question: "Which game that won the game of the year 2022?",
-        answers: [
-            { text: "Horizon Forbidden West", correct: false },
-            { text: "God of War: Ragnarok", correct: false },
-            { text: "Elden Ring", correct: true },
-            { text: "Overwatch 2", correct: false },
-        ]
-    },
-    {
-        question: "Which company that owns Activision-Blizzard?",
-        answers: [
-            { text: "Sony", correct: false },
-            { text: "Amazon", correct: false },
-            { text: "Nintendo", correct: false },
-            { text: "Microsoft", correct: true },
-        ]
-    },
-    {
-        question: "What is Amicia's real surname?",
-        answers: [
-            { text: "De Rune", correct: true },
-            { text: "De Stone", correct: false },
-            { text: "De Slab", correct: false },
-            { text: "De Bread", correct: false },
-        ]
-    },
-    {
-        question: "What is the first video game console?",
-        answers: [
-            { text: "Atari 2600", correct: false },
-            { text: "Nintendo 64", correct: false },
-            { text: "The Magnavox Odyssey", correct: true },
-            { text: "Xbox 360", correct: false },
-        ]
-    },
-    {
-        question: "Which is the most successful PlayStation 3 ONLY exclusive game?",
-        answers: [
-            { text: "Infamous", correct: false },
-            { text: "Killzone 2", correct: false },
-            { text: "Uncharted", correct: false },
-            { text: "The Last of Us", correct: true },
-        ]
-    },
-];
-
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("nxt-btn");
+const questionElement = document.getElementById('question');
+const answerButtons = document.getElementById('answer-buttons');
+const nextButton = document.getElementById('nxt-btn');
 
 let currentQuestionIndex = 0;
 let score = 0;
+let questions;
 
 function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
-    nextButton.innerHTML = "Next";
+    nextButton.innerHTML = 'Next';
     showQuestion();
 }
 
+function getQuestions() {
+    fetch('assets/js/questions.json')
+        .then((res) => res.json())
+        .then((result) => {
+            questions = result;
+            startQuiz();
+        });
+}
+
+/**
+ * Shows the next question and populates answers
+ */
 function showQuestion() {
     resetState();
-    let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+    const currentQuestion = questions[currentQuestionIndex];
+    const questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + '.' + currentQuestion.question;
 
     currentQuestion.answers.forEach((answer) => {
-        const button = document.createElement("button");
+        const button = document.createElement('button');
         button.innerHTML = answer.text;
-
-        button.classList.add("btn");
+        button.classList.add('btn');
 
         answerButtons.appendChild(button);
         if (answer.correct) {
             button.dataset.correct = answer.correct;
         }
-        button.addEventListener("click", selectAnswer);
+        button.addEventListener('click', selectAnswer);
     });
 }
 
+// Resets the game state
 function resetState() {
-    nextButton.style.display = "none";
+    nextButton.style.display = 'none';
     while (answerButtons.firstChild) {
         answerButtons.removeChild(answerButtons.firstChild);
     }
@@ -107,27 +54,27 @@ function resetState() {
 
 function selectAnswer(e) {
     const selectedBtn = e.target;
-    const isCorrect = selectedBtn.dataset.correct === "true";
+    const isCorrect = selectedBtn.dataset.correct === 'true';
     if (isCorrect) {
-        selectedBtn.classList.add("correct");
+        selectedBtn.classList.add('correct');
         score++;
     } else {
-        selectedBtn.classList.add("incorrect");
+        selectedBtn.classList.add('incorrect');
     }
-    Array.from(answerButtons.children).forEach(button => {
-        if (button.dataset.correct === "true") {
-            button.classList.add("correct");
+    Array.from(answerButtons.children).forEach((button) => {
+        if (button.dataset.correct === 'true') {
+            button.classList.add('correct');
         }
         button.disabled = true;
     });
-    nextButton.style.display = "block";
+    nextButton.style.display = 'block';
 }
 
 function showScore() {
     resetState();
     questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
-    nextButton.innerHTML = "Play Again";
-    nextButton.style.display = "block";
+    nextButton.innerHTML = 'Play Again?';
+    nextButton.style.display = 'block';
 }
 
 function handleNextButton() {
@@ -139,7 +86,7 @@ function handleNextButton() {
     }
 }
 
-nextButton.addEventListener("click", () => {
+nextButton.addEventListener('click', () => {
     if (currentQuestionIndex < questions.length) {
         handleNextButton();
     } else {
@@ -147,4 +94,4 @@ nextButton.addEventListener("click", () => {
     }
 });
 
-startQuiz();
+getQuestions();
